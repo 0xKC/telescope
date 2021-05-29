d60895ff76365beb8b49f0c412020db9fc34445a
0xKC <kavurikrishna.chaitanya@gmail.com>
2021-05-29 01:16:12 +0100

    added auth service

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
-
+import {HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+ private state,token,username : string;
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
 }
